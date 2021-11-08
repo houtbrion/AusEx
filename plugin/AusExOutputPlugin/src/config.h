466bb0f4ddd19ab88676cf1001762f2bb68d0e3c
houtbrion <houtbrion@gmail.com>
2021-11-09 07:34:35 +0900

    add definition of WiFiNINA
    Arduino MKR系列ボードへの対応を追加

--- plugin/AusExOutputPlugin/src/config.h
+++ plugin/AusExOutputPlugin/src/config.h
@@ -2,10 +2,11 @@
 #define __USE_SERIAL__          /* ログ出力先にシリアルを使う */
 //define __USE_SOFT_SERIAL__     /* ログ出力先にソフトシリアルを使う */
 //#define __USE_ETHERNET_CLIENT__ /* ログ出力先にTCPコネクション(server側)を使う */
-//#define __USE_WIFI_CLIENT__ /* ログ出力先にTCPコネクション(server側)を使う */
+#define __USE_WIFI_CLIENT__ /* ログ出力先にTCPコネクション(server側)を使う */
 //#define __USE_HTTP_CLIENT__     /* ログ出力先にHTTP Clientを使う */
 //#define __USE_MQTT_CLIENT__     /* ログ出力先にHTTP Clientを使う */
 
 //#define __USE_RTC__             /* ログの時刻情報の取得にRTCを使う */
+//#define __WIFI_NINA__           /* 強制的にWiFi_Nina.hをロードする場合 */
 
 
